647967ab62760fd036312b66f1a56fe2e3c70897
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-16 13:43:29 +0300

    requirements changes and setup gitignore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sensors.AFRICA API
 
 API to save data from deployed sensors in Africa.
+
+pip install -d git+https://github.com/CodeForAfricaLabs/sensors.AFRICA-AQ-api/ #feinstaub
--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('VERSION', "r") as f:
+  version = f.read().strip()
+
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
 setuptools.setup(
-    name="sensors_africa",
-    version="0.0.1",
+    name='sensors_africa',
+    version=version,
     author="CodeForAfrica",
-    description="Api to save and access data from deployed sensors.",
+    author_email="hello@codeforafrica.org",
+    description="API to save and access data from deployed sensors in Africa.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CodeForAfricaLabs/sensors.AFRICA-api",
