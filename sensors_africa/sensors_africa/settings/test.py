07027e830520baba088347901ff981038b0d79f4
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-17 13:13:45 +0300

    cleanup settings

--- sensors_africa/sensors_africa/settings/test.py
+++ sensors_africa/sensors_africa/settings/test.py
@@ -6,7 +6,7 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sensors_africa_test',
+        'NAME': 'test_sensorsafrica',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
