69a835e67c25bffbc9c794eb8b6cfce0d007a8d6
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-21 11:14:58 +0300

    cleanup

--- sensors_africa/tests/sensors_tests.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import json
-
-# def test_api_can_get_sensor_list(db, client, location):
-#   response = client.get('/v1/sensor', follow=True, format='json')
-
-#   assert response.status_code == 200
-#   sensors = json.loads(response.content)
-#   assert sensors['count'] == 1
