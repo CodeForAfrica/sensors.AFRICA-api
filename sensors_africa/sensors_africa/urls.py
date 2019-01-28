0d728e0daef89499dff508195e5dd4e638235620
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-28 14:58:05 +0300

    redirect to docs for index route

--- sensors_africa/sensors_africa/urls.py
+++ sensors_africa/sensors_africa/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 
+from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from rest_framework.documentation import include_docs_urls
@@ -33,6 +34,9 @@ urlpatterns = [
     url(r'^v1/', include(accounts_api_v1)),
     url(r'^get-auth-token/', obtain_auth_token),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    url(r'^$', RedirectView.as_view(url='/docs/', permanent=False)),
     url(r'^docs/', include_docs_urls(title='sensors.Africa API')),
+
     url(r'^adddevice/', AddSensordeviceView.as_view(), name='adddevice'),
 ] + staticfiles_urlpatterns()
