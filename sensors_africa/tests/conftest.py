492eb5724ac86681a4e52b249d98c11917b399cb
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-28 08:17:53 +0300

    created tests

--- sensors_africa/tests/conftest.py
+++ sensors_africa/tests/conftest.py
@@ -3,15 +3,24 @@ import pytest
 
 from django.core.management import call_command
 
-from feinstaub.sensors.models import Sensor, SensorLocation, SensorType, Node
+from feinstaub.sensors.models import (
+    Sensor, 
+    SensorLocation, 
+    SensorType, 
+    Node,
+    SensorData,
+    SensorDataValue
+)
 
+import datetime
+from django.utils import timezone
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command("loaddata", "auth.json")
-        call_command("loaddata", "sensortypes.json")
-        call_command("loaddata", "sensors.json")
+        # call_command("loaddata", "sensortypes.json")
+        # call_command("loaddata", "sensors.json")
 
 
 @pytest.fixture
@@ -35,8 +44,7 @@ def location():
 
 @pytest.fixture
 def sensor_type():
-    st, x = SensorType.objects.get_or_create(uid="A", name="B",
-                                             manufacturer="C")
+    st, x = SensorType.objects.get_or_create(uid="a", name="b", manufacturer="c")
     return st
 
 
@@ -53,3 +61,56 @@ def sensor(logged_in_user, sensor_type, node):
     s, x = Sensor.objects.get_or_create(node=node,
                                         sensor_type=sensor_type)
     return s
+
+@pytest.fixture
+def locations():
+    return SensorLocation.objects.bulk_create([
+        SensorLocation(city='Dar es Salaam', description='active'),
+        SensorLocation(city='Bagamoyo', description='inactive'),
+        SensorLocation(city='Mombasa', description='inactive'),
+        SensorLocation(city='Nairobi', description='inactive'),
+    ])
+
+@pytest.fixture
+def nodes(logged_in_user, locations):
+    return Node.objects.bulk_create([ 
+        Node(uid='0', owner=logged_in_user, location=locations[0]),
+        Node(uid='1', owner=logged_in_user, location=locations[1]),
+        Node(uid='2', owner=logged_in_user, location=locations[2]),
+        Node(uid='3', owner=logged_in_user, location=locations[3]),
+    ])
+
+@pytest.fixture
+def sensors(sensor_type, nodes):
+    return Sensor.objects.bulk_create([
+        Sensor(node=nodes[0], sensor_type=sensor_type),
+        # Inactive with last data push beyond active threshold
+        Sensor(node=nodes[1], sensor_type=sensor_type),
+        # Inactive without any data
+        Sensor(node=nodes[2], sensor_type=sensor_type),
+        Sensor(node=nodes[3], sensor_type=sensor_type),
+    ])
+
+@pytest.fixture
+def sensordata(sensors, locations):
+    now = timezone.now()
+    below_active_threshold_time = now - datetime.timedelta(minutes=40)
+    data =  SensorData.objects.bulk_create([
+        SensorData(sensor=sensors[0], location=locations[0]),
+        SensorData(sensor=sensors[1], location=locations[0]),
+    ])
+
+    data[1].update_modified = False
+    data[1].modified = below_active_threshold_time
+    data[1].save()
+
+    return data
+
+@pytest.fixture
+def datavalues(sensors, sensordata):
+    return SensorDataValue.objects.bulk_create([
+        SensorDataValue(sensordata=sensordata[0], value="1", value_type="P1"),
+        SensorDataValue(sensordata=sensordata[0], value="1", value_type="P2"),
+        SensorDataValue(sensordata=sensordata[1], value="2", value_type="humidity"),
+        SensorDataValue(sensordata=sensordata[1], value="2", value_type="temperature"),
+    ])
