e6804e406dd0a89d6815ca425a505416ccad99e8
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-30 09:39:15 +0300

    fix tests

--- sensors_africa/tests/test_getting_readings.py
+++ sensors_africa/tests/test_getting_readings.py
@@ -5,38 +5,41 @@ import pytz
 class TestGettingData:
 
     def test_getting_all_readings(self, client, datavalues):
-        response = client.get('/v2/readings/', format='json')
+        response = client.get('/v2/air/readings/', format='json')
         assert response.status_code == 200
 
         data = response.json()
 
-        assert len(data) == 8
+        assert len(data) == 11
 
 
     def test_getting_all_readings_by_city(self, client, datavalues):
-        response = client.get('/v2/readings/?city=Dar es Salaam', format='json')
+        response = client.get('/v2/air/readings/?city=Dar es Salaam', format='json')
         assert response.status_code == 200
 
         data = response.json()
 
-        assert len(data) == 8
+        assert len(data) == 10
 
 
-    def test_getting_all_readings(self, client, datavalues):
-        response = client.get('/v2/readings/now/?city=Dar es Salaam', format='json')
+    def test_getting_current_readings_by_city(self, client, datavalues):
+        response = client.get('/v2/air/readings/now/?city=Dar es Salaam', format='json')
         assert response.status_code == 200
 
         data = response.json()
 
         assert data["average"] == 4.5
         assert data["max"] == 8.0
-        assert data["average"] == 1.0
+        assert data["min"] == 1.0
 
 
-    def test_getting_all_readings(self, client, datavalues):
-        response = client.get('/v2/readings/now/', format='json')
+    def test_getting_current_readings_all_cities(self, client, datavalues):
+        response = client.get('/v2/air/readings/now/', format='json')
         assert response.status_code == 200
 
         data = response.json()
 
-        assert len(data) == 4
+        assert not data["Dar es Salaam"] == None
+        assert not data["Bagamoyo"] == None
+        assert not data["Nairobi"] == None
+        assert not data["Mombasa"] == None
