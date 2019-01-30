3f86f5cfe48be3ae4c10a3656b9080347ad3546f
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-30 08:47:51 +0300

    implimented logic to get readings

--- sensors_africa/api/data/base/router.py
+++ sensors_africa/api/data/base/router.py
@@ -1,11 +1,15 @@
 from rest_framework import routers
 
+from django.conf.urls import include, url
+
 from .views import (
   ReadingsView,
+  ReadingsNowView
 )
 
 router = routers.DefaultRouter()
 
+router.register(r'now', ReadingsNowView, basename='now')
 router.register(r'', ReadingsView)
 
 api_urls = router.urls
--- sensors_africa/api/data/base/views.py
+++ sensors_africa/api/data/base/views.py
@@ -6,6 +6,9 @@ from django.db.models import Q, Subquery
 
 from feinstaub.sensors.models import SensorData, SensorDataValue, SensorLocation
 
+import datetime
+from django.utils import timezone
+
 class SensorDataValueSerializer(serializers.ModelSerializer):
 
 	class Meta:
@@ -32,3 +35,37 @@ class ReadingsView(mixins.ListModelMixin,
 			sensor_data = SensorData.objects.filter(location=SensorLocation.objects.get(city=city))
 			return SensorDataValue.objects.filter(sensordata__in=sensor_data)
 		return SensorDataValue.objects.all()
+
+from rest_framework.response import Response
+from django.db.models import Avg, Max, Min, FloatField, Case, When, Q
+from django.db.models.functions import Cast
+
+class ReadingsNowView(viewsets.GenericViewSet):
+
+	def get_location_stats(self, location):
+		sensor_data = SensorData.objects.filter(location=location)
+
+		lte = timezone.now()
+		gte = lte - datetime.timedelta(24 * 60)
+
+		stats = SensorDataValue.objects\
+		.filter(sensordata__in=sensor_data, created__lte=lte, created__gte=gte, value_type='P2')\
+		.annotate(float_value=Case(
+			When(~Q(value_type='timestamp'), then=Cast('value', FloatField())),
+			output_field=FloatField(),
+			))\
+			.aggregate(average=Avg('float_value'), max=Max('float_value'), min=Min('float_value'))
+		
+		return stats
+
+	def list(self, request):
+		city = request.query_params.get('city')
+		if city:
+			stats = self.get_location_stats(SensorLocation.objects.get(city=city))
+		else:
+			stats = {}
+			for location in SensorLocation.objects.all():
+				stats[location.city] = self.get_location_stats(location)
+
+			return Response(stats)
+		return Response({})
