6ff32dbf85c83f77ece7aea4444644f141868457
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-01 13:42:53 +0300

    ALLOWED_HOSTS

--- sensorsafrica/settings.py
+++ sensorsafrica/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("SENSOSAFRICA_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
