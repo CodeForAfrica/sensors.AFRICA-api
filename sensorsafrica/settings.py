84b01e04faf46b27bb893ceb5a6398adb5ad5aa1
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-04 11:31:45 +0300

    remove use built in password validators

--- sensorsafrica/settings.py
+++ sensorsafrica/settings.py
@@ -95,29 +95,6 @@ DATABASE_URL = os.getenv(
 DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
 
 
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.\
-        password_validation.UserAttributeSimilarityValidator"
-    },
-    {
-        "NAME": "django.contrib.auth.\
-        password_validation.MinimumLengthValidator"
-    },
-    {
-        "NAME": "django.contrib.auth.\
-        password_validation.CommonPasswordValidator"
-    },
-    {
-        "NAME": "django.contrib.auth.\
-        password_validation.NumericPasswordValidator"
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
