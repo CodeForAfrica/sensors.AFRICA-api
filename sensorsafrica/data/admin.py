0e0d058071c56504e94352bbfb8c0dcc90238d77
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-11 15:46:31 +0300

    readonly data stats

--- sensorsafrica/data/admin.py
+++ sensorsafrica/data/admin.py
@@ -5,6 +5,17 @@ from .models import SensorDataStat
 
 @admin.register(SensorDataStat)
 class SensorDataStatAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "node",
+        "sensor",
+        "location",
+        "city_slug",
+        "value_type",
+        "average",
+        "maximum",
+        "minimum",
+        "date",
+    ]
     search_fields = ["city_slug", "value_type"]
     list_display = [
         "node",
@@ -20,3 +31,23 @@ class SensorDataStatAdmin(admin.ModelAdmin):
         "modified",
     ]
     list_filter = ["date", "node", "sensor", "location"]
+
+    def get_actions(self, request):
+        actions = super(SensorDataStatAdmin, self).get_actions(request)
+        del actions["delete_selected"]
+        return actions
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        pass
+
+    def delete_model(self, request, obj):
+        pass
+
+    def save_related(self, request, form, formsets, change):
+        pass
