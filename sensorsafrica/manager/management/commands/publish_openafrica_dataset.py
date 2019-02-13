5837e3ee83b89f291fb697ea8af0821f1a2ee2d9
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-13 13:04:00 +0300

    script to create resource and update resource

--- sensorsafrica/manager/management/commands/publish_openafrica_dataset.py
+++ sensorsafrica/manager/management/commands/publish_openafrica_dataset.py
@@ -11,39 +11,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         openAFRICA_API_KEY = os.environ.get("openAFRICA_API_KEY")
         openAFRICA_URL = "https://africaopendata.org"
+        folder = os.environ.get("ARCHIVE_FOLDER_PATH", "/opt/code/archive")
+
         ckan = ckanapi.RemoteCKAN(openAFRICA_URL, apikey=openAFRICA_API_KEY)
-        package_name = "sensors.AFRICA Air Quality Archive"
-        package_title = "sensors.AFRICA Air Quality Archive"
-        folder = "/opt/code/archive"
 
-        try:
-            package = ckan.action.package_create(name=package_name, title=package_title)
-        except ckanapi.ValidationError as e:
-            if e.error_dict["__type"] == "Validation Error" and (
-                e.error_dict["name"] == ["That URL is already in use."]
-            ):
-                package = ckan.action.package_show(id=package_name)
-            else:
-                raise
+        package = ckan.action.package_show(id="sensorsafrica-airquality-archive")
+        resources = package["resources"]
 
-        for filename in os.listdir(folder):
-            path = os.path.join(folder, filename)
-            extension = os.path.splitext(filename)[1][1:].upper()
-            resource_name = "sensors.AFRICA Air Quality Data {extension}".format(
-                extension=extension
-            )
-            r = requests.post(
-                openAFRICA_URL + "/api/action/resource_create",
-                data={
-                    "package_id": package["id"],
-                    "name": resource_name,
-                    "format": extension,
-                    "url": "upload",
-                },
-                headers={"Authorization": openAFRICA_API_KEY},
-                files=[("upload", open(path))],
-            )
+        for subfolder in os.listdir(folder):
+            path = os.path.join(folder, subfolder)
+            for filename in os.listdir(path):
+                path = os.path.join(path, filename)
+                extension = os.path.splitext(filename)[1][1:].upper()
 
-            if r.status_code != 200:
-                print("Error while creating resource: {0}".format(r.content))
-                break
+                resource_name = filename.replace("_", " ").replace(".", " ").upper()
+
+                resource = list(filter(lambda resource: resource["name"] == resource_name, resources))
+                if resource:
+                    resource = ckan.action.resource_update(
+                        id=resource[0]["id"],
+                        url="upload",
+                        upload=open(path),
+                    )
+                else:
+                    resource = ckan.action.resource_create(
+                        package_id=package["id"],
+                        name=resource_name,
+                        format=extension,
+                        url="upload",
+                        upload=open(path),
+                    )
