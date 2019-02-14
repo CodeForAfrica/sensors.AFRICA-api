9a72b763e0f4e69fa62d20eb9f94007d4c7d7b7c
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-14 13:11:58 +0300

    monthly archive files

--- sensorsafrica/manager/management/commands/upload_to_ckan.py
+++ sensorsafrica/manager/management/commands/upload_to_ckan.py
@@ -1,69 +1,145 @@
+import calendar
+import datetime
 import os
-from urllib import request
-
-import boto3
-import ckanapi
-from django.core.management import BaseCommand
 import time
+
+import ckanapi
+import pytz
+from django.core.management import BaseCommand
+from django.db.models import Max, Min
+from django.utils.text import slugify
+from feinstaub.sensors.models import SensorData, SensorLocation
 
 
 class Command(BaseCommand):
     help = ""
 
-    def add_arguments(self, parser):
-        parser.add_argument("--from_s3")
-
     def handle(self, *args, **options):
         openAFRICA_API_KEY = os.environ.get("openAFRICA_API_KEY")
+        openAFRICA_CFA_ID = os.environ.get("openAFRICA_CFA_ID")
         openAFRICA_URL = "https://africaopendata.org"
 
         ckan = ckanapi.RemoteCKAN(openAFRICA_URL, apikey=openAFRICA_API_KEY)
 
-        package = ckan.action.package_show(id="sensorsafrica-airquality-archive")
-        resources = package["resources"]
+        city_queryset = (
+            SensorLocation.objects.all()
+            .values_list("city", flat=True)
+            .order_by("city")
+            .distinct("city")
+        )
+        for city in city_queryset.iterator():
+            if not city:
+                continue
 
-        if options.get("from_s3"):
-            bucket = self._s3_archive_bucket()
-
-            for obj in bucket.objects.all():
-                filename = obj.key.split("/")[-1]
-
-                region = os.environ.get("AWS_REGION")
-                bucket_name = os.environ.get("AWS_BUCKET_NAME")
-                url = "http://s3-%s.amazonaws.com/%s/%s" % (
-                    region,
-                    bucket_name,
-                    obj.key,
+            try:
+                package = ckan.action.package_create(
+                    owner_org=openAFRICA_CFA_ID,
+                    name="sensorsafrica-airquality-archive-" + slugify(city),
+                    title="sensors.AFRICA Air Quality Archive " + city,
+                )
+            except ckanapi.ValidationError:
+                package = ckan.action.package_show(
+                    id="sensorsafrica-airquality-archive-" + slugify(city)
                 )
 
-                self.create_or_update_resource(
-                    filename, resources, ckan, request.urlopen(url), package
+            resources = package["resources"]
+
+            start_date = None
+            for resource in resources:
+                date = resource["name"].replace("Sensor Data Archive", "")
+                if date:
+                    date = datetime.datetime.strptime(date, "%B %Y ")
+                    if not start_date or date > start_date:
+                        start_date = date
+
+            timestamp = SensorData.objects.filter(location__city=city).aggregate(
+                Max("timestamp"), Min("timestamp")
+            )
+
+            if not start_date:
+                start_date = timestamp["timestamp__min"].replace(
+                    day=1, hour=0, minute=0, second=0, microsecond=0
+                )
+            end_date = timestamp["timestamp__max"].replace(
+                day=1, hour=0, minute=0, second=0, microsecond=0
+            )
+
+            start_date = start_date.replace(tzinfo=pytz.UTC)
+            end_date = end_date.replace(tzinfo=pytz.UTC)
+
+            date = start_date
+            while date <= end_date:
+                qs = (
+                    SensorData.objects.filter(
+                        location__city=city,
+                        timestamp__month=date.month,
+                        timestamp__year=date.year,
+                    )
+                    .values(
+                        "sensor__id",
+                        "sensor__sensor_type__name",
+                        "location__id",
+                        "location__latitude",
+                        "location__longitude",
+                        "timestamp",
+                        "sensordatavalues__value_type",
+                        "sensordatavalues__value",
+                    )
+                    .order_by("timestamp")
                 )
 
-                time.sleep(5)
-        else:
-            folder = os.environ.get("ARCHIVE_FOLDER_PATH", "/opt/code/archive")
-
-            for subfolder in os.listdir(folder):
-                path = os.path.join(folder, subfolder)
-                for filename in os.listdir(path):
-                    path = os.path.join(path, filename)
-
-                    self.create_or_update_resource(
-                        filename, resources, ckan, open(path), package
+                if qs.exists():
+                    resource_name = "{month} {year} Sensor Data Archive".format(
+                        month=calendar.month_name[date.month], year=date.year
                     )
 
-    def create_or_update_resource(self, filename, resources, ckan, upload, package):
-        extension = os.path.splitext(filename)[1][1:].upper()
+                    self._write_file(filepath="./temp/file.csv", qs=qs)
+                    self._create_or_update_resource(
+                        resource_name, "./temp/file.csv", resources, ckan, package
+                    )
 
-        resource_name = filename.replace("_", " ").replace(".", " ").upper()
+                    # Don't DDOS openAFRICA
+                    time.sleep(20)
+
+                # Incriment month
+                date = datetime.datetime(
+                    day=1,
+                    month=(date.month + 1) % 12,
+                    year=date.year + date.month // 12,
+                    tzinfo=pytz.UTC,
+                )
+
+    @staticmethod
+    def _write_file(filepath, qs):
+        with open(filepath, "w") as fp:
+            fp.write(
+                "sensor_id;sensor_type;location;lat;lon;timestamp;value_type;value\n"
+            )
+            for sd in qs:
+                s = ";".join(
+                    [
+                        str(sd["sensor__id"]),
+                        sd["sensor__sensor_type__name"],
+                        str(sd["location__id"]),
+                        "{:.3f}".format(sd["location__latitude"]),
+                        "{:.3f}".format(sd["location__longitude"]),
+                        sd["timestamp"].isoformat(),
+                        sd["sensordatavalues__value_type"],
+                        sd["sensordatavalues__value"],
+                    ]
+                )
+                fp.write(s + "\n")
+
+    @staticmethod
+    def _create_or_update_resource(resource_name, filepath, resources, ckan, package):
+        extension = "CSV"
 
         resource = list(
             filter(lambda resource: resource["name"] == resource_name, resources)
         )
         if resource:
             resource = ckan.action.resource_update(
-                id=resource[0]["id"], url="upload", upload=upload
+                id=resource[0]["id"], url="upload", upload=open(filepath)
             )
         else:
             resource = ckan.action.resource_create(
@@ -71,23 +147,5 @@ class Command(BaseCommand):
                 name=resource_name,
                 format=extension,
                 url="upload",
-                upload=upload,
+                upload=open(filepath),
             )
-
-    @staticmethod
-    def _s3_archive_bucket():
-        bucket_name = os.environ.get("AWS_BUCKET_NAME")
-        access_key = os.environ.get("AWS_ACCESS_KEY")
-        secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-        region = os.environ.get("AWS_REGION")
-
-        session = boto3.Session(
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_access_key,
-            region_name=region,
-        )
-        s3 = session.resource("s3")
-
-        bucket = s3.Bucket(bucket_name)
-
-        return bucket
