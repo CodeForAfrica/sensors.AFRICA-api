a5632ef7a27daf6fb486176d4155b373b4058cfd
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-15 15:47:11 +0300

    [crontab] tests

--- sensorsafrica/management/commands/cron.py
+++ sensorsafrica/management/commands/cron.py
@@ -1,5 +1,6 @@
+import shelve
+import uuid
 import crontab
-import os
 from django.core.management import BaseCommand
 
 
@@ -8,25 +9,39 @@ class Command(BaseCommand):
     help = "Install or reinstall cronjobs"
 
     def add_arguments(self, parser):
-        parser.add_argument("--breadcrumb")
-        parser.add_argument("--dokku_appname")
+        parser.add_argument("--breadcrumb", default=str(uuid.uuid4()))
+        parser.add_argument("--dokku_appname", default=False)
+        parser.add_argument("--clear", default=False)
 
     def handle(self, *args, **options):
         tab = crontab.CronTab(user=True)
 
         breadcrumb = options["breadcrumb"]
         dokku_appname = options["dokku_appname"]
+        clear = options["clear"]
 
-        count = len(list(tab.find_comment(breadcrumb)))
+        if not dokku_appname and not clear:
+            return
 
-        if count:
-            tab.remove_all(comment=breadcrumb)
-            tab.write()
+        with shelve.open("crons") as db:
+            if "breadcrumb" in db.keys():
+                previous_breadcrumb = db["breadcrumb"]
 
-        tab.new(
-            command="dokku enter {dokku_appname} web python3 manage.py calculate_data_statistics >> /var/log/cron.log 2>&1".format(dokku_appname=dokku_appname),
-            comment=breadcrumb,
-        ).setall("0 * * * *")
-        tab.write()
+                count = len(list(tab.find_comment(previous_breadcrumb)))
 
+                if count:
+                    tab.remove_all(comment=previous_breadcrumb)
+                    tab.write()
+
+            if dokku_appname:
+                tab.new(
+                    command="dokku enter {dokku_appname} web python3 manage.py calculate_data_statistics >> /var/log/cron.log 2>&1".format(
+                        dokku_appname=dokku_appname
+                    ),
+                    comment=breadcrumb,
+                ).setall("0 * * * *")
+
+                tab.write()
+
+                db["breadcrumb"] = breadcrumb
         print(tab.render())
--- sensorsafrica/tests/test_crontab.py
+++ sensorsafrica/tests/test_crontab.py
@@ -11,3 +11,25 @@ class TestSensorDataPushFull:
         count = len(list(tab.find_comment("sensorsafrica-test")))
 
         assert count == 1
+
+    def test_crontab_reintalled(self):
+        call_command("cron", dokku_appname="sensorsafrica-test-reinstall", breadcrumb="sensorsafrica-test-reinstall")
+
+        tab = crontab.CronTab(user=True)
+
+        prev_jobs = len(list(tab.find_comment("sensorsafrica-test")))
+        current_jobs = len(list(tab.find_comment("sensorsafrica-test-reinstall")))
+
+        assert prev_jobs == 0
+        assert current_jobs == 1
+
+    def test_crontab_clear(self):
+        call_command("cron", clear=True)
+
+        tab = crontab.CronTab(user=True)
+
+        prev_jobs = len(list(tab.find_comment("sensorsafrica-test")))
+        current_jobs = len(list(tab.find_comment("sensorsafrica-test-reinstall")))
+
+        assert prev_jobs == 0
+        assert current_jobs == 0
