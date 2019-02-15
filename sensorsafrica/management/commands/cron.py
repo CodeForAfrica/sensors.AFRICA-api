22f2f3854cfcce09ec745126a001d7d05b821a80
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-15 13:09:41 +0300

    [crontab] setup cronjob using command

---
+++ sensorsafrica/management/commands/cron.py
@@ -0,0 +1,25 @@
+from django.core.management import BaseCommand, get_commands
+import crontab
+
+BREADCRUMB = "sensorsafrica"
+
+
+class Command(BaseCommand):
+    help = ""
+
+    def handle(self, *args, **options):
+        tab = crontab.CronTab(user=True)
+
+        count = len(list(tab.find_comment(BREADCRUMB)))
+
+        if count:
+            tab.remove_all(comment=BREADCRUMB)
+            tab.write()
+
+        tab.new(
+            "dokku enter sensorsafrica-staging web python3 manage.py calculate_data_statistics >> /var/log/cron.log 2>&1",
+            BREADCRUMB,
+        ).setall("0 * * * *")
+        tab.write()
+
+        print(tab.render())
