fea2b4ad05fecf0399fc4149db4b9be4a8a1897a
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-25 13:20:29 +0300

    setup celery

---
+++ sensorsafrica/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from celery import Celery
+from celery.schedules import crontab
+
+# Set sensorsafrica application settings module for sensorsafrica Celery instance
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensorsafrica.settings")
+
+# Create Celery instance and pass the project name
+# The instance is bound to the variable app
+app = Celery("sensorsafrica")
+
+# Pass config made of up values begging with the prefix of CELERY_ in settings.py
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Autodiscover tasks in tasks.py
+app.autodiscover_tasks()
