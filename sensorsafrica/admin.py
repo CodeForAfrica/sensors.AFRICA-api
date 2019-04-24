c94bb092234833a3af88e3c827bf2f354edf3afa
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-04-24 12:13:36 +0300

    Admin to show last data push for each sensor location (#37)
    * admin to see sensors last push date
    
    * fix lint
    
    * address pr comments

--- sensorsafrica/admin.py
+++ sensorsafrica/admin.py
@@ -2,6 +2,57 @@ from django.contrib import admin
 
 from .api.models import SensorDataStat, City
 
+from feinstaub.sensors.admin import (
+    SensorLocationAdmin,
+    SensorLocation,
+    SensorData,
+    Node,
+)
+
+import timeago
+import datetime
+import django.utils.timezone
+
+
+def last_data_received_at(self, obj):
+    then = (
+        SensorData.objects.filter(location=obj)
+        .values_list("timestamp", flat=True)
+        .last()
+    )
+    now = datetime.datetime.now(django.utils.timezone.utc)
+
+    if not then:
+        return "Unknown"
+
+    return "( %s ) %s" % (
+        timeago.format(then, now),
+        SensorData.objects.filter(location=obj)
+        .values_list("timestamp", flat=True)
+        .last(),
+    )
+
+
+def latitude_and_longitude(self, obj):
+    return "%s,%s" % (obj.latitude, obj.longitude)
+
+
+def node_UID(self, obj):
+    return Node.objects.filter(location=obj).values_list("uid", flat=True).first()
+
+
+SensorLocation._meta.verbose_name_plural = "Sensor Node Locations"
+SensorLocationAdmin.list_display = [
+    "node_UID",
+    "location",
+    "city",
+    "latitude_and_longitude",
+    "last_data_received_at",
+]
+SensorLocationAdmin.last_data_received_at = last_data_received_at
+SensorLocationAdmin.latitude_and_longitude = latitude_and_longitude
+SensorLocationAdmin.node_UID = node_UID
+
 
 @admin.register(SensorDataStat)
 class SensorDataStatAdmin(admin.ModelAdmin):
