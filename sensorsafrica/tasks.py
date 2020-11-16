6a1693fa2bbf18d7a17a1efcee6260deab98d047
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2020-11-16 10:14:49 +0100

    deactivate celery task

--- sensorsafrica/tasks.py
+++ sensorsafrica/tasks.py
@@ -2,9 +2,9 @@ from celery import shared_task
 from django.core.management import call_command
 
 
-@shared_task
-def calculate_data_statistics():
-    call_command("calculate_data_statistics")
+# @shared_task
+# def calculate_data_statistics():
+#     call_command("calculate_data_statistics")
 
 
 @shared_task
@@ -17,12 +17,12 @@ def cache_lastactive_nodes():
     call_command("cache_lastactive_nodes")
 
 
-@shared_task
-def cache_static_json_data():
-    call_command("cache_static_json_data", interval='5m')
+# @shared_task
+# def cache_static_json_data():
+#     call_command("cache_static_json_data", interval='5m')
 
 
-@shared_task
-def cache_static_json_data_1h_24h():
-    call_command("cache_static_json_data", interval='1h')
-    call_command("cache_static_json_data", interval='24h')
+# @shared_task
+# def cache_static_json_data_1h_24h():
+#     call_command("cache_static_json_data", interval='1h')
+#     call_command("cache_static_json_data", interval='24h')
