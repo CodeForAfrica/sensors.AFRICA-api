d2738f6296c2a2ed1b7ccb2b102957102013671e
khadija <khadijamahanga@y7mail.com>
2020-12-11 17:09:20 +0300

    params

--- sensorsafrica/api/v2/purpleair_service.py
+++ sensorsafrica/api/v2/purpleair_service.py
@@ -2,8 +2,9 @@ import os
 import requests
 
 def get_sensors():
+    query = {'fields':'sensor_index'}
     url = 'https://api.purpleair.com/v1/sensors?fields=sensor_index'
-    r = requests.get(url, headers={'Authorization':'Bearer %s' % 'access_token'})
+    r = requests.get(url, params=query, headers={'Authorization':'Bearer %s' % 'access_token'})
     results = r.json()
     sensors_id_list = []
     for i in range(len(results.data)):
