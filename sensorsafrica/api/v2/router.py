4ae22a42e1ff42d74c00b312295a8dacd7f2ce28
khadija <khadijamahanga@y7mail.com>
2020-12-11 17:11:45 +0300

    spelling

--- sensorsafrica/api/v2/router.py
+++ sensorsafrica/api/v2/router.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from django.conf.urls import url, include
 
-from .views import SensorDataStatView, CityView, NodesView, SensorsLocationView, SensorsTypeView, SensorsView
+from .views import SensorDataStatView, CityView, NodesView, SensorsLocationView, SensorTypeView, SensorsView
 
 data_router = routers.DefaultRouter()
 
@@ -21,8 +21,8 @@ sensors_router.register(r"", SensorsView, basename="sensors")
 sensors_location_router = routers.DefaultRouter()
 sensors_location_router.register(r"", SensorsLocationView, basename="location")
 
-sensors_type_router = routers.DefaultRouter()
-sensors_type_router.register(r"", SensorsTypeView, basename="sensors_type")
+sensor_type_router = routers.DefaultRouter()
+sensor_type_router.register(r"", SensorTypeView, basename="sensor_type")
 
 api_urls = [
     url(r"data/(?P<sensor_type>[air]+)/", include(data_router.urls)),
@@ -30,5 +30,5 @@ api_urls = [
     url(r"nodes/", include(nodes_router.urls)),
     url(r"locations/", include(sensors_location_router.urls)),
     url(r"sensors/", include(sensors_router.urls)),
-    url(r"sensors-type/", include(sensors_type_router.urls)),
+    url(r"sensor-type/", include(sensor_type_router.urls)),
 ]
