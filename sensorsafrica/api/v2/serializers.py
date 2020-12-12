976f524de715f5513c185e7dc1dfe82d7b8331bf
Clemence Kyara <clemence@codeforafrica.org>
2020-12-12 15:14:59 +0300

    Standard route names

--- sensorsafrica/api/v2/serializers.py
+++ sensorsafrica/api/v2/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from feinstaub.sensors.serializers import NestedSensorLocationSerializer, NestedSensorTypeSerializer
+from feinstaub.sensors.serializers import (
+    NestedSensorLocationSerializer,
+    NestedSensorTypeSerializer,
+)
 from feinstaub.sensors.models import Node, Sensor
 
 
@@ -24,42 +27,45 @@ class CitySerializer(serializers.Serializer):
     def get_label(self, obj):
         return "{}, {}".format(obj.name, obj.country)
 
+
 class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
-        fields = ('id', 'node', 'description', 'pin', 'sensor_type', 'public')
+        fields = ("id", "node", "description", "pin", "sensor_type", "public")
+
 
 class SensorLocationSerializer(NestedSensorLocationSerializer):
     class Meta(NestedSensorLocationSerializer.Meta):
-        fields = NestedSensorLocationSerializer.Meta.fields + \
-            (
-                'longitude',
-                'latitude',
-                'altitude',
-                'street_name',
-                'street_number',
-                'city',
-                'country',
-                'postalcode',
-                'traffic_in_area',
-                'oven_in_area',
-                'industry_in_area',
-                'owner',
-            )
+        fields = NestedSensorLocationSerializer.Meta.fields + (
+            "longitude",
+            "latitude",
+            "altitude",
+            "street_name",
+            "street_number",
+            "city",
+            "country",
+            "postalcode",
+            "traffic_in_area",
+            "oven_in_area",
+            "industry_in_area",
+            "owner",
+        )
+
+
 class NodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Node
         fields = (
-                'uid',
-                'owner',
-                'location',
-                'name',
-                'description',
-                'height',
-                'sensor_position',
-                'email',
-                'last_notify',
-                'indoor',
-                'inactive',
-                'exact_location',
-            )
+            "uid",
+            "owner",
+            "location",
+            "name",
+            "description",
+            "height",
+            "sensor_position",
+            "email",
+            "last_notify",
+            "indoor",
+            "inactive",
+            "exact_location",
+        )
