0339e4c072534e5aedbae755c81e3f245b4fcfe4
_ Kilemensi <kilemensi@users.noreply.github.com>
2021-01-21 13:17:13 +0300

    [Feature] Make v1/now return public data only (#86)
    * Recreate feinstaub NowView but with public sensors filter
    
    * Switch to custom NowView

--- sensorsafrica/api/v1/views.py
+++ sensorsafrica/api/v1/views.py
@@ -2,17 +2,52 @@ import datetime
 import pytz
 import json
 
-from rest_framework.exceptions import ValidationError
 
 from django.conf import settings
-from django.utils import timezone
-from dateutil.relativedelta import relativedelta
 from django.db.models import ExpressionWrapper, F, FloatField, Max, Min, Sum, Avg, Q
 from django.db.models.functions import Cast, TruncDate
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 from rest_framework import mixins, pagination, viewsets
+from rest_framework.exceptions import ValidationError
+
+from feinstaub.sensors.models import SensorData
+from feinstaub.sensors.serializers import NowSerializer
 
 from .serializers import SensorDataSerializer
-from feinstaub.sensors.models import SensorData
+
+class FilterView(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = SensorDataSerializer
+
+    def get_queryset(self):
+        sensor_type = self.request.GET.get("type", r"\w+")
+        country = self.request.GET.get("country", r"\w+")
+        city = self.request.GET.get("city", r"\w+")
+        return (
+            SensorData.objects.filter(
+                timestamp__gte=timezone.now() - datetime.timedelta(minutes=5),
+                sensor__sensor_type__uid__iregex=sensor_type,
+                location__country__iregex=country,
+                location__city__iregex=city,
+            )
+            .only("sensor", "timestamp")
+            .prefetch_related("sensordatavalues")
+        )
+
+
+class NowView(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Show all public sensors active in the last 5 minutes with newest value"""
+
+    permission_classes = []
+    serializer_class = NowSerializer
+    queryset = SensorData.objects.none()
+
+    def get_queryset(self):
+        now = timezone.now()
+        startdate = now - datetime.timedelta(minutes=5)
+        return SensorData.objects.filter(
+            sensor__public=True, modified__range=[startdate, now]
+        )
 
 
 class SensorDataView(mixins.ListModelMixin, viewsets.GenericViewSet):
@@ -20,31 +55,10 @@ class SensorDataView(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     def get_queryset(self):
         return (
-            SensorData.objects
-            .filter(
+            SensorData.objects.filter(
                 timestamp__gte=timezone.now() - datetime.timedelta(minutes=5),
-                sensor=self.kwargs["sensor_id"]
+                sensor=self.kwargs["sensor_id"],
             )
-            .only('sensor', 'timestamp')
-            .prefetch_related('sensordatavalues')
-        )
-
-
-class FilterView(mixins.ListModelMixin, viewsets.GenericViewSet):
-    serializer_class = SensorDataSerializer
-
-    def get_queryset(self):
-        sensor_type = self.request.GET.get('type', r'\w+')
-        country = self.request.GET.get('country', r'\w+')
-        city = self.request.GET.get('city', r'\w+')
-        return (
-            SensorData.objects
-            .filter(
-                timestamp__gte=timezone.now() - datetime.timedelta(minutes=5),
-                sensor__sensor_type__uid__iregex=sensor_type,
-                location__country__iregex=country,
-                location__city__iregex=city
-            )
-            .only('sensor', 'timestamp')
-            .prefetch_related('sensordatavalues')
+            .only("sensor", "timestamp")
+            .prefetch_related("sensordatavalues")
         )
