365a530d3cd7375f0c37eef0b98d8e6da1928f3e
esir <esirkings@gmail.com>
2021-02-01 17:28:41 +0300

    Temporaliry Remove iexact

--- sensorsafrica/api/v2/filters.py
+++ sensorsafrica/api/v2/filters.py
@@ -3,4 +3,4 @@ from feinstaub.sensors.views import SensorFilter
 class CustomSensorFilter(SensorFilter):
     class Meta(SensorFilter.Meta):
         # Pick the fields already defined and add the location__country field
-        fields = {**SensorFilter.Meta.fields, **{'location__country': ['iexact']}}
+        fields = {**SensorFilter.Meta.fields, **{'location__country': ['exact']}}
