1cfc40752a880368f32f9ed79395a5e265669bae
khadija <khadijamahanga@y7mail.com>
2021-02-01 15:14:58 +0300

    add last_notify filter on nodes, sensorfilter

--- sensorsafrica/api/v1/filters.py
+++ sensorsafrica/api/v1/filters.py
@@ -1,9 +1,47 @@
 import django_filters
 
-from feinstaub.sensors.models import Node
+from feinstaub.sensors.models import Node, SensorData
 
 class NodeFilter(django_filters.FilterSet):
     class Meta:
         model = Node
-        fields = {"location__country": ["exact"]}
+        fields = {
+            "location__country": ["iexact"],
+            "last_notify": ["gte", "lte"]}
+        filter_overrides = {
+            models.DateTimeField: {
+                'filter_class': django_filters.IsoDateTimeFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'gte',
+                },
+            },
+            models.DateTimeField: {
+                'filter_class': django_filters.IsoDateTimeFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'lte',
+                },
+            },
+        }
 
+
+class SensorFilter(django_filters.FilterSet):
+    class Meta:
+        model = SensorData
+        fields = {
+            "sensor": ["exact"],
+            "timestamp": ["gte", "lte"]
+            }
+        filter_overrides = {
+            models.DateTimeField: {
+                'filter_class': django_filters.IsoDateTimeFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'gte',
+                },
+            },
+            models.DateTimeField: {
+                'filter_class': django_filters.IsoDateTimeFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'lte',
+                },
+            },
+        }
