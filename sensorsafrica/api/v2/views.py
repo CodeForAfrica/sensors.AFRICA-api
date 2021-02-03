079ef0f7fde94bce533d82fad3bdf4b2b2a73692
khadija <khadijamahanga@y7mail.com>
2021-02-03 19:53:42 +0300

    function that counts online/offline nodes

--- sensorsafrica/api/v2/views.py
+++ sensorsafrica/api/v2/views.py
@@ -425,12 +425,22 @@ def meta_data(request):
     return Response({
         "sensor_networks": get_sensors_networks(),
         "nodes_count": nodes_count,
+        "online_nodes_count": get_online_nodes(),
+        "offline_nodes_count": get_offline_nodes(),
         "sensors_count": sensors_count,
         "sensor_data_count": sensor_data_count,
         "sensors_locations": sensors_locations,
         "database_size": database_size[0],
         "database_last_updated": database_last_updated,
     })
+
+def get_online_nodes():
+    nodes_count = Node.objects.filter(last_notify__gte=timezone.now() - datetime.timedelta(days=14)).count()
+    return nodes_count
+
+def get_offline_nodes():
+    nodes_count = Node.objects.filter(Q(last_notify__isnull=True) | Q(last_notify__lte=timezone.now() - datetime.timedelta(days=14))).count()
+    return nodes_count
 
 def get_sensors_networks():
     user = User.objects.filter(username=settings.NETWORKS_OWNER).first()
