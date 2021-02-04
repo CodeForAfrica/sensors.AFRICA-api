435102c4284b5b0b75d8540fe661f494aa9908ae
esir <esirkings@gmail.com>
2021-02-04 14:07:32 +0300

    Remove un used code

--- sensorsafrica/management/commands/add_city_names.py
+++ sensorsafrica/management/commands/add_city_names.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         geolocator = Nominatim(user_agent="sensors-api")
         all_nodes = Node.objects.filter(Q(location__city=None) |  Q(location__city=''))
-        cities = []
+        
         for node in all_nodes:
             try:
                 location = geolocator.reverse(f"{node.location.latitude}, {node.location.longitude}")
