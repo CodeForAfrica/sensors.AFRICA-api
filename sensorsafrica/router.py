d3c9e0f7223a9a4601d7326ff526a7c9c7dc6303
esir <esirkings@gmail.com>
2022-11-02 06:39:48 +0300

    Use the default database for user authentication queries

--- sensorsafrica/router.py
+++ sensorsafrica/router.py
@@ -1,6 +1,20 @@
 class ReplicaRouter:
+    route_app_labels = {'auth', 'sessions',}
+
     def db_for_read(self, model, **hints):
+        if model._meta.app_label in self.route_app_labels:
+            return 'default'
         return "read_replica"
     
     def db_for_write(self, model, **hints):
         return "default"
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Relations between objects are allowed if both objects are
+        in the primary/replica pool.
+        """
+        db_set = {'default', 'read_replica'}
+        if obj1._state.db in db_set and obj2._state.db in db_set:
+            return True
+        return None
