694bdf00744d1154da0d91b37134e4d5f031b66d
gideon maina <gmainapro@yahoo.com>
2023-06-19 17:30:34 +0300

    CoreAPI schema implementation

--- sensorsafrica/settings.py
+++ sensorsafrica/settings.py
@@ -81,6 +81,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema" 
 }
 
 TEMPLATES = [
