49a4297feba22efb5f36263a89a45650aad4ec84
Giddie <gmainapro@yahoo.com>
2026-07-07 11:24:51 +0300

    [test CI] fix: rewrite cache_lastactive_nodes raw SQL to Django ORM

--- sensorsafrica/management/commands/cache_lastactive_nodes.py
+++ sensorsafrica/management/commands/cache_lastactive_nodes.py
@@ -1,6 +1,9 @@
 from django.core.management import BaseCommand
 
 from django.db import connection
+from django.db.models import Max
+from django.utils import timezone
+from datetime import timedelta
 from sensorsafrica.api.models import Node, SensorLocation, LastActiveNodes
 
 
@@ -8,28 +11,21 @@ class Command(BaseCommand):
     help = ""
 
     def handle(self, *args, **options):
-        with connection.cursor() as cursor:
-            cursor.execute(
-                """
-                    SELECT
-                        sn.id,
-                        sn.location_id,
-                        MAX("timestamp") AS last_active_date
-                    FROM
-                        sensors_sensordata sd
-                        INNER JOIN sensors_sensor s ON s.id = sd.sensor_id
-                            INNER JOIN sensors_node sn ON sn.id = s.node_id
-                            INNER JOIN sensors_sensordatavalue sv ON sv.sensordata_id = sd.id
-                            AND sv.value_type in ('P1', 'P2')
-                        WHERE
-                            "timestamp" >= now() - INTERVAL '5 min'
-                        GROUP BY
-                            sn.id
-                """)
-            latest = cursor.fetchall()
-            for data in latest:
-                LastActiveNodes.objects.update_or_create(
-                    node=Node(pk=data[0]),
-                    location=SensorLocation(pk=data[1]),
-                    defaults={"last_data_received_at": data[2]},
-                )
+        from feinstaub.sensors.models import SensorData, SensorDataValue
+
+        five_min_ago = timezone.now() - timedelta(minutes=5)
+
+        latest = (
+            SensorDataValue.objects
+            .filter(value_type__in=('P1', 'P2'))
+            .filter(sensordata__timestamp__gte=five_min_ago)
+            .values('sensordata__sensor__node__id', 'sensordata__sensor__node__location__id')
+            .annotate(last_active_date=Max('sensordata__timestamp'))
+        )
+
+        for data in latest:
+            LastActiveNodes.objects.update_or_create(
+                node=Node(pk=data['sensordata__sensor__node__id']),
+                location=SensorLocation(pk=data['sensordata__sensor__node__location__id']),
+                defaults={"last_data_received_at": data['last_active_date']},
+            )
